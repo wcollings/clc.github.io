e20313e8ff904aa529d1c548c00d5ae57b8aa4a6
Will Collings <william.collings@rockets.utoledo.edu>
2021-07-19 20:37:30 -0400

    dynamic loading
    Making a new branch for dynamic loading of pages, since I think I figured that out. These files are unused here, but I made them before thinking so there's here now

--- dirs.py
+++ dirs.py
@@ -45,3 +45,43 @@ def get_files(songs_only=False):
 			else:
 				all_files[name]=[path]
 	return all_files
+
+def get_dir_struct(songs_only=False):
+	ds={}
+	for root,dirs,files in os.walk("."):
+		for dir in dirs:
+			if ".git" in root:
+				continue
+			if ".idea" in root:
+				continue
+			if dir in ignore_folders:
+				continue
+			r=root[2:]
+			if r in ds:
+				ds[r].append(dir)
+			else:
+				ds[r]=[dir]
+			if r=='':
+				if not dir in ds:
+					ds[dir]={}
+			else:
+				if not r in ds:
+					ds[r]={}
+				ds[r]={dir}
+		for file in files:
+			if file in ignore_files:
+				continue
+			if not ".html" in file:
+				continue
+			if "index" in file:
+				continue
+			if root==".":
+				continue
+			path=g.splitall(root)
+			ds[path[1]][path[2]]
+			continue
+
+	print(ds)
+
+#get_dir_struct()
+print(get_folders())
--- js/load.js
+++ js/load.js
@@ -1,42 +1,64 @@
-	 function load_artist(link)
-	 {
-		temp="https://www.celticlyricscorner.site/"+link
-      fetch(temp)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function (data) {
-        fill_page(data);
-      })
-    }
-	 function fill_page(data)
-	 {
-		document.getElementsByTagName("title")[0].innerHTML=data["artist"] +" Lyrics";
-		var artist_tags=document.getElementsByTagName("art_name");
-		for (var i=0; i < artist_tags.length; ++i)
-		{
-			artist_tags[i].innerHTML=data["artist"];
-		}
-		var albums=document.getElementById("albums")
-		var iH='<td id="lc"> Albums </td><td>';
-		for (var i=0; i < data["albums"].length; ++i)
-		{
-			iH+=data["albums"][i][0]+' - ';
-			iH+='<a href="' + data["albums"][i][2] + '/' + data["albums"][i][2] + '.html"> ';
-			iH+=data["albums"][i][1] + '</a> <br>';
-		}
-		iH+="</td>";
-		albums.innerHTML=iH;
 
-		var img=document.getElementById("img");
-		var art_pic=new Image("150");
-		art_pic.src="breabach.jpg";
-		art_pic.alt=data["artist"];
-		var lc=document.createElement("td");
-		var rc=document.createElement("td");
-		rc.innerHTML="<H1>" + data["artist"] +"</H1>";
-		lc.setAttribute("id","lc");
-		lc.appendChild(art_pic);
-		img.appendChild(lc);
-		img.appendChild(rc);
-	 }
+function getUrlParam(parameter, defaultvalue){
+    var urlparameter = defaultvalue;
+    if(window.location.href.indexOf(parameter) > -1){
+        urlparameter = getUrlVars()[parameter];
+        }
+    return urlparameter;
+}
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+function load_artist(link)
+{
+	var link=getUrlParam("artist","None");
+	var temp="https://www.celticlyricscorner.site/"+link +"/artist_info.json";
+	fetch(temp)
+	.then(function(response) {
+		return response.json();
+	})
+	.then(function (data) {
+		fill_page(data);
+	})
+	}
+	function fill_page(data)
+	{
+	document.getElementsByTagName("title")[0].innerHTML=data["artist"] +" Lyrics";
+	var artist_tags=document.getElementsByTagName("art_name");
+	for (var i=0; i < artist_tags.length; ++i)
+	{
+		artist_tags[i].innerHTML=data["artist"];
+	}
+	var albums=document.getElementById("albums")
+	var iH='<td id="lc"> Albums </td><td>';
+	for (var i=0; i < data["albums"].length; ++i)
+	{
+		iH+=data["albums"][i][0]+' - ';
+		iH+='<a href="' + data["albums"][i][2] + '/' + data["albums"][i][2] + '.html"> ';
+		iH+=data["albums"][i][1] + '</a> <br>';
+	}
+	iH+="</td>";
+	albums.innerHTML=iH;
+
+	var img=document.getElementById("img");
+	var art_pic=new Image("150");
+	art_pic.src="breabach.jpg";
+	art_pic.alt=data["artist"];
+	var lc=document.createElement("td");
+	var rc=document.createElement("td");
+	rc.innerHTML="<H1>" + data["artist"] +"</H1>";
+	lc.setAttribute("id","lc");
+	lc.appendChild(art_pic);
+	img.appendChild(lc);
+	img.appendChild(rc);
+
+	if ("members" in data)
+	{
+		var mem_col=document.getElementById("members");
+		mem_col.innerHTML='<td id="lc"> Members </td><td> ' + data["members"] + '</td>';
+	}
+}
